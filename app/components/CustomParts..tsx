ff01786ba4454bb17405d0b835466ce82e160ad4
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-11-09 23:39:02 +0600

    added Customparts section

---
+++ app/components/CustomParts..tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import customImage from '../../app/assets/image.svg';
+
+const CustomParts = () => {
+  return (
+    <div className=" py-8">
+      <div className=" flex flex-col md:flex-row items-center bg-white shadow-lg overflow-hidden">
+        {/* Image */}
+        <div className="w-full md:w-1/2 md:h-[403px] relative">
+            <Image
+                src={customImage}
+                alt="Description of the image"
+                className="object-cover w-full h-full "
+            />
+            <div className="absolute inset-0 bg-[#256bbb] opacity-60"></div>
+        </div>
+
+
+        {/* text */}
+        <div className="w-full h-[403px] md:w-1/2 p-6 flex flex-col justify-center bg-[#0487E2]">
+          <h2 className="text-4xl font-semibold mb-4 text-white">Get Exclusive Custom Parts</h2>
+          <p className="text-base mb-6 text-white font-light">
+            Sky Gears has been the supplier that aircraft builders, owners, <br /> pilots, and aviation businesses have depended on since 1965. <br /> We carry a wide selection of aircraft parts.
+          </p>
+          <button className="bg-white text-black py-2 px-4 rounded-md hover:bg-blue-600 hover:text-white self-start">
+            Apply Now
+          </button>
+        </div>
+        </div>
+    </div>
+  );
+};
+
+export default CustomParts;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import CustomParts from './components/CustomParts.'
 
 const page = () => {
   return (
     <>
       <Header/> 
       <Hero/>
+      <CustomParts/>
     </>
   )
 }
