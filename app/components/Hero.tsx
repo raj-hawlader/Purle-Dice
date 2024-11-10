804332888982ac7af80b3968ebb99c247d2e8d3a
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-11-10 22:59:28 +0600

    fix: Hero section laout

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,26 +8,15 @@ import img1 from '../../app/assets/image (1).png';
 import img2 from '../../app/assets/image (1).png';
 import img3 from '../../app/assets/image (1).png';
 import img4 from '../../app/assets/image (1).png';
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
-
-const PrevArrow = (props: any) => {
-  const { onClick } = props;
-  return (
-    <button
-      onClick={onClick}
-      className="absolute left-[-40px] top-[50%] transform -translate-y-1/2 text-2xl text-[#BEBEBE] hover:text-[#226dc4]">
-      <FaChevronLeft />
-    </button>
-  );
-};
 
 const NextArrow = (props: any) => {
   const { onClick } = props;
   return (
     <button
       onClick={onClick}
-      className="absolute right-[-40px] top-[50%] transform -translate-y-1/2 text-2xl text-[#BEBEBE] hover:text-[#226dc4]">
+      className="absolute right-1 md:-right-8 top-1/2 transform -translate-y-1/2 text-lg md:text-2xl text-[#BEBEBE] hover:text-[#226dc4]">
       <FaChevronRight />
     </button>
   );
@@ -42,7 +31,6 @@ const Hero = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
   };
 
@@ -50,11 +38,18 @@ const Hero = () => {
     <div className="container mx-auto px-4 pt-5 relative">
       <Slider {...settings}>
         {images.map((image, index) => (
-          <div key={index} className="w-full">
-            <Image
-              src={image}
-              alt={`Carousel image ${index + 1}`}
-              className="w-full h-auto rounded-md"/>
+          <div key={index} className="flex justify-center">
+            <div className="w-full flex justify-center">
+              <Image
+                src={image}
+                alt={`Carousel image ${index + 1}`}
+                className="rounded-md object-cover text-center"
+                width={1296}
+                height={478}
+                sizes="(max-width: 768px) 90vw, (max-width: 1024px) 80vw, 1296px"
+                style={{ width: "100%", height: "auto", maxWidth: "1296px", maxHeight: "478px" }}
+              />
+            </div>
           </div>
         ))}
       </Slider>
