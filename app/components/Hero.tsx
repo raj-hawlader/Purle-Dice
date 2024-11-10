1c0997b28631b01c8a7bccaa35e190ad5f842c42
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-11-10 12:03:35 +0600

    fix: carousel issue

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,35 +1,62 @@
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
-import { GoDotFill } from 'react-icons/go';
-import Image from 'next/image';
-import heroImage from '../../app/assets/image (1).png';
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
+import img1 from '../../app/assets/image (1).png';
+import img2 from '../../app/assets/image (1).png';
+import img3 from '../../app/assets/image (1).png';
+import img4 from '../../app/assets/image (1).png';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+const PrevArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <button
+      onClick={onClick}
+      className="absolute left-[-40px] top-[50%] transform -translate-y-1/2 text-2xl text-[#BEBEBE] hover:text-[#0487E2]">
+      <FaChevronLeft />
+    </button>
+  );
+};
+
+const NextArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <button
+      onClick={onClick}
+      className="absolute right-[-40px] top-[50%] transform -translate-y-1/2 text-2xl text-[#BEBEBE] hover:text-[#0487E2]">
+      <FaChevronRight />
+    </button>
+  );
+};
 
 const Hero = () => {
+  const images = [img1, img2, img3, img4];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
   return (
-    <div className='container mx-auto flex justify-center items-center h-full px-4 md:px-[38px] pt-5'>
-      <div className='flex flex-col items-center w-full'>
-
-        {/* hero image */}
-        <div className='relative flex items-center w-full'>
-          <Image
-            src={heroImage}
-            alt="Description of the image"
-            className="w-full h-auto rounded-md"
-          />
-
-          {/* right arrow */}
-          <button className='absolute right-2 sm:right-[-40px] text-3xl'>
-            <MdOutlineKeyboardArrowRight className='opacity-25 hover:opacity-100'/>
-          </button>
-        </div>
-
-        {/* dots */}
-        <div className='flex mt-1'>
-          <GoDotFill className='text-[#D9D9D9] text-2xl md:text-2xl lg:text-4xl cursor-pointer hover:text-[#4493FA]' />
-          <GoDotFill className='text-[#4493FA] text-2xl md:text-2xl lg:text-4xl cursor-pointer hover:text-[#4493FA]' />
-          <GoDotFill className='text-[#D9D9D9] text-2xl md:text-2xl lg:text-4xl cursor-pointer hover:text-[#4493FA]' />
-          <GoDotFill className='text-[#D9D9D9] text-2xl md:text-2xl lg:text-4xl cursor-pointer hover:text-[#4493FA]' />
-        </div>
-      </div>
+    <div className="container mx-auto px-4 pt-5 relative">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className="w-full">
+            <Image
+              src={image}
+              alt={`Carousel image ${index + 1}`}
+              className="w-full h-auto rounded-md"/>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
