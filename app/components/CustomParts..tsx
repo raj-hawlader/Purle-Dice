443796f6ecfc2cbac7e2e7012b146bb04147c8ac
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-11-10 22:31:44 +0600

    fix: CustomParts layout

--- app/components/CustomParts..tsx
+++ app/components/CustomParts..tsx
@@ -3,30 +3,37 @@ import customImage from '../../app/assets/image.svg';
 
 const CustomParts = () => {
   return (
-    <div className=" py-8">
-      <div className=" flex flex-col md:flex-row items-center bg-white shadow-lg overflow-hidden">
-        {/* Image */}
-        <div className="w-full md:w-1/2 md:h-[403px] relative">
+    <div className="py-8">
+      <div className="container mx-auto max-w-[1440px]">
+        <div className="flex flex-col md:flex-row items-center bg-white shadow-lg overflow-hidden h-auto md:h-[403px]">
+          
+          {/* Image */}
+          <div className="w-full md:w-1/2 h-[250px] md:h-[403px] relative">
             <Image
-                src={customImage}
-                alt="Description of the image"
-                className="object-cover w-full h-full "
+              src={customImage}
+              alt="Description of the image"
+              className="object-cover w-full h-full"
             />
             <div className="absolute inset-0 bg-[#256bbb] opacity-60"></div>
-        </div>
+          </div>
 
+          {/* Text */}
+          <div className="w-full md:w-1/2 h-[250px] md:h-[403px] p-4 md:p-6 flex flex-col justify-center bg-[#0487E2]">
+            <h2 className="text-2xl md:text-4xl font-semibold mb-4 text-white font-exo text-center md:text-left">
+              Get Exclusive Custom Parts
+            </h2>
+            <p className="text-sm md:text-base mb-6 text-white font-light font-exo text-center md:text-left">
+              Sky Gears has been the supplier that aircraft builders, owners,
+              <br className="hidden md:block" /> pilots, and aviation businesses have depended on since 1965. 
+              <br className="hidden md:block" /> We carry a wide selection of aircraft parts.
+            </p>
+            <button className="bg-white text-black font-exo text-sm py-3 px-5 font-semibold rounded-md hover:bg-blue-600 hover:text-white self-center md:self-start">
+              Apply Now
+            </button>
+          </div>
 
-        {/* text */}
-        <div className="w-full h-[403px] md:w-1/2 p-6 flex flex-col justify-center bg-[#0487E2]">
-          <h2 className="text-4xl font-semibold mb-4 text-white font-exo">Get Exclusive Custom Parts</h2>
-          <p className="text-base mb-6 text-white font-light font-exo">
-            Sky Gears has been the supplier that aircraft builders, owners, <br /> pilots, and aviation businesses have depended on since 1965. <br /> We carry a wide selection of aircraft parts.
-          </p>
-          <button className="bg-white text-black font-exo text-sm py-3 px-5 font-semibold rounded-md hover:bg-blue-600 hover:text-white self-start">
-            Apply Now
-          </button>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
